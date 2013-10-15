fea0942567c728a703f962372086573f72d70618
Edouard Lafargue <edouard@lafargue.name>
2013-10-14 23:00:00 -0700

    Implement draft full value display in numeric display for Fluke289

--- server/public/js/instruments/fluke289/display_numeric.js
+++ server/public/js/instruments/fluke289/display_numeric.js
@@ -29,11 +29,58 @@ window.Fluke289NumView = Backbone.View.extend({
     },
     
     showInput: function(data) {
-        if (typeof(data.value) == 'undefined')
-            return;
+        if (typeof(data.value) != 'undefined') {
+            $('#livereading', this.el).html(data.value + "&nbsp;" + this.linkManager.driver.mapUnit(data.unit));
+        }
         
-        $('#livereading', this.el).html(data.value + "&nbsp;" + this.linkManager.driver.mapUnit(data.unit));
-
+        if (data.reading != undefined) {
+            // This is a complete reading, we have to get all readings from it
+            var readings = data.reading.readings;
+            $('#live').empty();
+            $('#primary').empty();
+            $('#seconary').empty();
+            $('#offset').empty();
+            $('#minimum').empty();
+            $('#average').empty();
+            $('#maximum').empty();
+            for (var i = 0; i < readings.length; i++) {
+                var reading = readings[i];
+                if (reading.readingState == "NORMAL") {
+                    // There are several areas where we can draw:
+                    var location = "primary";
+                    switch (reading.readingID) {
+                            case "LIVE":
+                                location="live"
+                                break;
+                            case "PRIMARY":
+                                location = "primary";
+                                break;
+                            case "SECONDARY":
+                                location = "secondary";
+                                break;
+                            case "TEMP_OFFSET":
+                                location = "temp_offset";
+                                reading.readingValue = "Offset: " + reading.readingValue;
+                                break;
+                            case "MINIMUM":
+                                location = "minimum";
+                                reading.readingValue = "Minimum: " + reading.readingValue;
+                                break;
+                            case "MAXIMUM":
+                                location = "maximum";
+                                reading.readingValue = "Maximum: " + reading.readingValue;
+                                break;
+                            case "AVERAGE":
+                                location = "average";
+                                reading.readingValue = "Average: " + reading.readingValue;
+                                break;
+                    }
+                    $('#' + location).html(reading.readingValue + "&nbsp;" + this.linkManager.driver.mapUnit(reading.baseUnit));
+                }
+           }
+        }
+        
+        
         // Update statistics:
         var sessionDuration = (new Date().getTime() - this.sessionStartStamp)/1000;
         $('#sessionlength',this.el).html(utils.hms(sessionDuration));
