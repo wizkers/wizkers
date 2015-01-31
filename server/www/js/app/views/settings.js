ca1f57fae514c6d3db830db16c0556dc0ad62f29
Ed <edouard@lafargue.name>
2015-01-30 23:00:00 -0800

    Only show "Firmware upgrade" button on devices that support it. Fixes Issue #71

--- server/www/js/app/views/settings.js
+++ server/www/js/app/views/settings.js
@@ -22,70 +22,72 @@
  *
  * @author Edouard Lafargue, ed@lafargue.name
  */
-define(function(require) {
-    
+define(function (require) {
+
     "use strict";
-    
-    var $        = require('jquery'),
-        _       = require('underscore'),
+
+    var $ = require('jquery'),
+        _ = require('underscore'),
         Backbone = require('backbone'),
         template = require('js/tpl/SettingsView.js');
-    
+
     require('bootstrap');
-    
+
     function deleteDB(dbObj) {
-    try {
+        try {
 
-        var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB ;
+            var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
 
-        var dbreq = indexedDB.deleteDatabase(dbObj.id);
-        dbreq.onsuccess = function (event) {
-            var db = event.result;
-            console.log("indexedDB: " + dbObj.id + " deleted");
+            var dbreq = indexedDB.deleteDatabase(dbObj.id);
+            dbreq.onsuccess = function (event) {
+                var db = event.result;
+                console.log("indexedDB: " + dbObj.id + " deleted");
+            }
+            dbreq.onerror = function (event) {
+                console.error("indexedDB.delete Error: " + event.message);
+            }
+        } catch (e) {
+            console.error("Error: " + e.message);
+            //prefer change id of database to start ont new instance
+            dbObj.id = dbObj.id + "." + guid();
+            console.log("fallback to new database name :" + dbObj.id)
         }
-        dbreq.onerror = function (event) {
-            console.error("indexedDB.delete Error: " + event.message);
-        }
-    }
-    catch (e) {
-        console.error("Error: " + e.message);
-        //prefer change id of database to start ont new instance
-        dbObj.id = dbObj.id + "." + guid();
-        console.log("fallback to new database name :" + dbObj.id)
-    }
     }
 
 
     return Backbone.View.extend({
 
-        initialize:function () {
-        },
+        initialize: function () {},
 
-        render:function () {        
+        render: function () {
             $(this.el).html(template(this.model.toJSON()));
-            
+
             // Depending on the runmode, we can display additional info
             if (vizapp.type == "chrome") {
                 $("#chromesettings", this.el).append("<br><h4>Storage</h4>");
-                chrome.storage.local.getBytesInUse(function(used){
+                chrome.storage.local.getBytesInUse(function (used) {
                     $("#chromesettings", this.el).append("<p>" + used + " bytes used out of a " +
-                                                         chrome.storage.local.QUOTA_BYTES + " bytes quota.</p>");
-                    });
+                        chrome.storage.local.QUOTA_BYTES + " bytes quota.</p>");
+                });
+                
+                if (instrumentManager.getCaps().indexOf("Upgrader") > -1) {
+                    $('#device_upgrade', this.el).show();
+                }
             }
-            
+
             return this;
         },
 
         events: {
-            "change"      : "change",
+            "change": "change",
             "click #reset": "reset_settings",
-            "click #reset_storage" : "reset_storage_ask",
-            "click #do-delete" : "do_reset_storage",
+            "click #reset_storage": "reset_storage_ask",
+            "click #do-delete": "do_reset_storage",
             "click .cpmcolor": "selectColor",
             "click #device_upgrade": "device_upgrade"
         },
 
-        change: function(event) {
+        change: function (event) {
             console.log("Settings changed");
             // Apply the change to the model
             var target = event.target;
@@ -96,65 +98,71 @@ define(function(require) {
             this.render();
 
         },
-        
-        device_upgrade: function() {
+
+        device_upgrade: function () {
             var caps = instrumentManager.getCaps();
             if (caps.indexOf("Upgrader") > -1) {
                 router.navigate('upgrader/' + instrumentManager.getInstrument().id, true);
             }
         },
 
-        selectColor: function(event) {
+        selectColor: function (event) {
             var self = this;
             console.log("Selected color: " + event.target.title);
             this.model.set('cpmcolor', event.target.title);
-            this.model.save({success: function() { self.render; } });
+            this.model.save({
+                success: function () {
+                    self.render;
+                }
+            });
         },
 
-        reset_settings: function() {
+        reset_settings: function () {
             var self = this;
             // Clear our global settings/state:
             this.model.clear().set(this.model.defaults);
             this.model.save(null, {
-                    success: function(model) {
-                       utils.showAlert('Success', 'Settings cleared', 'alert-success');
-                        self.render();
-                       return true;
-                    },
-                    error: function () {
-                        utils.showAlert('Error', 'An error occurred while trying to clear the settings', 'alert-error');
+                success: function (model) {
+                    utils.showAlert('Success', 'Settings cleared', 'alert-success');
+                    self.render();
+                    return true;
+                },
+                error: function () {
+                    utils.showAlert('Error', 'An error occurred while trying to clear the settings', 'alert-error');
                 }
             });
             return false;
         },
-        
-        reset_storage_ask: function() {
-            $('#deleteConfirm',this.el).modal('show');
+
+        reset_storage_ask: function () {
+            $('#deleteConfirm', this.el).modal('show');
         },
-        
-        do_reset_storage: function() {
-            
-            var dbreq = indexedDB.open("wizkers-logs",1);
+
+        do_reset_storage: function () {
+
+            var dbreq = indexedDB.open("wizkers-logs", 1);
             dbreq.onsuccess = function (event) {
                 var db = event.result;
                 console.log(event);
                 console.log(dbreq);
             };
             dbreq.onerror = function (event) {
-                        console.error("indexedDB.delete Error: " + event.message);
+                console.error("indexedDB.delete Error: " + event.message);
             };
-            
-            
-            
-            
+
+
+
+
             var ins = instrumentManager.getInstrument();
             if (ins != null) {
                 linkManager.closeInstrument(ins.id);
             }
             chrome.storage.local.clear();
             instrumentManager.clear();
-            $('#deleteConfirm',self.el).modal('hide');
-            router.navigate('home', {trigger: true});
+            $('#deleteConfirm', self.el).modal('hide');
+            router.navigate('home', {
+                trigger: true
+            });
         },
 
 
