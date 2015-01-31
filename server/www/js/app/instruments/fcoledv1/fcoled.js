2e52b5913440e3de428f44c49aa33747a4d85ab5
Ed <edouard@lafargue.name>
2015-01-30 23:00:00 -0800

    Add firmware upgrade capabilities on the Fried Circuits OLED backpack.

--- server/www/js/app/instruments/fcoledv1/fcoled.js
+++ server/www/js/app/instruments/fcoledv1/fcoled.js
@@ -22,85 +22,104 @@
  * @author Edouard Lafargue, ed@lafargue.name
  */
 
-define(function(require) {
+define(function (require) {
     "use strict";
 
     var driver_frontend = require('app/instruments/fcoledv1/driver_frontend');
+    var uploader_frontend = require('app/instruments/usbgeiger/uploader_frontend');
 
-    return function() {
-    
+    return function () {
+
         // Helper function: get driver capabilites.
         // returns a simple array of capabilities    
-        this.getCaps = function() {
-            return ["LiveDisplay",  "NumDisplay", "LogView"];
+        this.getCaps = function () {
+            return ["LiveDisplay", "NumDisplay", "LogView", "Upgrader"];
         };
-                    
+
         // Return the type of data reading that this instrument generates. Can be used
         // by output plugins to accept data from this instrument or not.
-        this.getDataType = function() {
-                    return [ "voltage", "current", "power", "energy" ];
+        this.getDataType = function () {
+            return ["voltage", "current", "power", "energy"];
         }
+
+        this.getUpgrader = function (arg, callback) {
+            require(['app/instruments/usbgeiger/upgrader'], function (view) {
+                callback(new view(arg));
+            });
+        };
+
 
 
         // This has to be a backbone view
-        this.getSettings = function(arg, callback) {
-            require(['app/instruments/fcoledv1/settings'], function(view) {
+        this.getSettings = function (arg, callback) {
+            require(['app/instruments/fcoledv1/settings'], function (view) {
                 callback(new view(arg));
             });
         };
 
         // This has to be a Backbone view
         // This is the full screen live view (not a small widget)
-        this.getLiveDisplay = function(arg, callback) {
-            require(['app/instruments/fcoledv1/display_live'], function(view) {
+        this.getLiveDisplay = function (arg, callback) {
+            require(['app/instruments/fcoledv1/display_live'], function (view) {
                 callback(new view(arg));
             });
         };
 
         // This is a Backbone view
         // This is a numeric display
-        this.getNumDisplay = function(arg, callback) {
-            require(['app/instruments/fcoledv1/display_numeric'], function(view) {
+        this.getNumDisplay = function (arg, callback) {
+            require(['app/instruments/fcoledv1/display_numeric'], function (view) {
                 callback(new view(arg));
             });
         };
 
         // A diagnostics/device setup screen
-        this.getDiagDisplay = function(arg, callback) {
+        this.getDiagDisplay = function (arg, callback) {
             return null;
         };
 
         // A smaller widget (just a graph)
-        this.getLiveWidget = function(arg, callback) {
+        this.getLiveWidget = function (arg, callback) {
             return null;
         };
 
         // The instrument driver (browser-side)
-        this.getDriver = function() {
+        this.getDriver = function () {
             return new driver_frontend();
         };
-        
+
+        this.getUploader = function () {
+            return new uploader_frontend();
+        };
+
+
         // This is a browser implementation of the backend driver, when we
         // run the app fully in-browser on as a Cordova native app.
-        this.getBackendDriver = function(arg, callback) {
-            require(['app/instruments/fcoledv1/driver_backend'], function(driver) {
+        this.getBackendDriver = function (arg, callback) {
+            require(['app/instruments/fcoledv1/driver_backend'], function (driver) {
+                callback(new driver(arg));
+            });
+        };
+
+        this.getBackendUploaderDriver = function (arg, callback) {
+            require(['app/instruments/usbgeiger/uploader_backend'], function (driver) {
                 callback(new driver(arg));
             });
         };
 
         // Return a Backbone view which is a mini graph
-        this.getMiniLogview = function(arg) {
+        this.getMiniLogview = function (arg) {
             return null;
         };
 
         // Return a device log management view
-        this.getLogManagementView = function(arg) {
+        this.getLogManagementView = function (arg) {
             return null;
         }
 
         // Render a log (or list of logs) for the device.
-        this.getLogView = function(arg, callback) {
-            require(['app/instruments/fcoledv1/display_log'], function(view) {
+        this.getLogView = function (arg, callback) {
+            require(['app/instruments/fcoledv1/display_log'], function (view) {
                 callback(new view(arg));
             });
         }
