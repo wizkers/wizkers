8f097a8500abd402e6cdc61f338d07ba7d12409c
Ed <edouard@lafargue.name>
2015-10-12 23:00:00 +0900

    New instrument: Safecast bGeigie (through Bluetooth module).  Connects to the bGeigie, visualizes and logs. Initial log export functionality (simple write to file on Android).

---
+++ server/www/js/app/instruments/bgeigie/display_log.js
@@ -0,0 +1,309 @@
+/**
+ * (c) 2015 Edouard Lafargue, ed@lafargue.name
+ *
+ * This file is part of Wizkers.
+ *
+ * Wizkers is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wizkers is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Wizkers.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Log view for the Onyx.
+ *
+ * Our model is a collection of Logs
+ *
+ * @author Edouard Lafargue, ed@lafargue.name
+ */
+
+define(function (require) {
+    "use strict";
+
+    var $ = require('jquery'),
+        _ = require('underscore'),
+        Backbone = require('backbone'),
+        simpleplot = require('app/lib/flotplot'),
+        utils = require('app/utils'),
+        fileutils = require('app/lib/fileutils'),
+        template = require('js/tpl/instruments/bgeigie/LogView.js');
+
+    // Load the flot library & flot time plugin:
+    require('flot');
+    require('flot_time');
+    require('flot_resize');
+    require('flot_selection');
+
+    return Backbone.View.extend({
+
+        initialize: function () {
+            var self = this;
+
+            this.deviceLogs = this.collection;
+            this.packedData = null;
+            this.previousPoint = null;
+
+            // Need this to make sure "render" will always be bound to our context.
+            // -> required for the _.after below.
+            _.bindAll(this, "render");
+
+            // We will pass this when we create plots, this is the global
+            // config for the look and feel of the plot
+            this.plotSettings = {
+                selectable: true,
+                vertical_stretch: true,
+                points: 0,
+                plot_options: {
+                    xaxis: {
+                        mode: "time",
+                        show: true,
+                        timeformat: "%Y.%m.%d<br>%H:%M",
+                        timezone: settings.get("timezone"),
+                    },
+                    yaxis: {},
+                    grid: {
+                        hoverable: true,
+                        clickable: true
+                    },
+                    legend: {
+                        position: "ne"
+                    },
+                    colors: this.palette,
+                }
+            };
+
+            this.plotOverviewSettings = {
+                selectable: true,
+                vertical_stretch: false,
+                points: 0,
+                plot_options: {
+                    xaxis: {
+                        mode: "time",
+                        show: true,
+                        timeformat: "%m.%d",
+                        timezone: settings.get("timezone"),
+                    },
+                    yaxis: {},
+                    colors: this.palette,
+                }
+            };
+
+            // My own nice color palette:
+            this.palette = ["#e27c48", "#5a3037", "#f1ca4f", "#acbe80", "#77b1a7", "#858485", "#d9c7ad"];
+
+            // Now fetch all the contents, then render
+            var renderGraph = _.after(this.deviceLogs.length, this.render);
+            this.deviceLogs.each(function (log) {
+                log.entries.fetch({
+                    success: renderGraph,
+                    xhr: function () {
+                        var xhr = $.ajaxSettings.xhr();
+                        xhr.onprogress = self.handleProgress;
+                        return xhr;
+                    }
+                });
+            });
+        },
+
+        handleProgress: function (e) {
+            $('#loadtext').html("Loaded: " + e.loaded + " bytes");
+        },
+
+        events: {
+            "click .resetZoom": "resetZoom",
+            "click #cpmscale": "cpmScaleToggle",
+            "click #send-to-api": "uploadLog"
+        },
+
+        resetZoom: function () {
+            delete this.ranges;
+            this.plot.plotOptions = this.plotSettings.plot_options;
+            this.plot.addPlot();
+            this.plot.redraw();
+            return false;
+        },
+
+        cpmScaleToggle: function (event) {
+            return;
+            var change = {};
+            if (event.target.checked) {
+                change["cpmscale"] = "log";
+            } else {
+                change["cpmscale"] = "linear";
+            }
+            settings.set(change);
+            this.render();
+            this.addPlot();
+
+        },
+
+        render: function () {
+            var self = this;
+            console.log('Main render of Log details view');
+
+            $(this.el).html(template());
+
+            this.addPlot();
+            if (this.packedData == null || this.packedData.length == 0)
+                this.packedData = this.packData();
+
+            if (this.packedData.length == 0)
+                return;
+            return this;
+        },
+
+        onClose: function () {
+            console.log("Log view closing...");
+
+            // Remove the window resize bindings on our plots:
+            this.plot.onClose();
+            if (this.overview)
+                this.overview.onClose();
+        },
+
+        uploadLog: function () {
+            var self = this;
+            fileutils.newLogFile("safecast-upload.log", function (file) {
+                file.createWriter(function (fileWriter) {
+                    // fileWriter.seek(fileWriter.length);
+                    for (var i = 0; i < self.deviceLogs.length; i++) {
+                        var currentLog = self.deviceLogs.at(i);
+                        var entries = currentLog.entries;
+
+                        var index = 0;
+                        var write = function (evt) {
+                            if (index == entries.length)
+                                return;
+                            var entry = entries.at(index);
+                            index++;
+                            var data = entry.get('data');
+                            // Sometimes, we get entries without a valid reading, detect this
+                            if (data.nmea) {
+                                fileWriter.write(data.nmea + '\n');
+                            }
+                        };
+                        fileWriter.onwrite = write;
+                        write(0);
+
+                    }
+                }, function (e) {
+                    console.log(e);
+                });
+            });
+        },
+
+        addPlot: function () {
+            var self = this;
+
+            if (this.deviceLogs == null ||  this.deviceLogs.length == 0)
+                return;
+
+            $('#log_size', this.el).html(this.deviceLogs.getOverallLength());
+            $('#log_start', this.el).html(new Date(this.deviceLogs.getLogsStart()).toString());
+            $('#log_end', this.el).html(new Date(this.deviceLogs.getLogsEnd()).toString());
+
+            this.plot = new simpleplot({
+                model: this.model,
+                settings: this.plotSettings
+            });
+            if (this.plot != null) {
+                $('.geigerchart', this.el).empty().append(this.plot.el);
+                this.plot.render();
+            }
+
+            // We don't create an overview on small screens, does not make sense
+            if (!(utils.checkBreakpoint('sm') || utils.checkBreakpoint('xs'))) {
+                this.overview = new simpleplot({
+                    model: this.model,
+                    settings: this.plotOverviewSettings
+                });
+            }
+
+            if (this.overview != null) {
+                $('#overview', this.el).empty().append(this.overview.el);
+                this.overview.render();
+            } else {
+                $('#overview-container', this.el).empty();
+            }
+
+            // Restore current zoom level if it exists:
+            if (this.ranges) {
+                this.plot.plotOptions = $.extend(true, {}, this.plotSettings.plot_options, {
+                    xaxis: {
+                        min: this.ranges.xaxis.from,
+                        max: this.ranges.xaxis.to
+                    },
+                    yaxis: {
+                        min: this.ranges.yaxis.from,
+                        max: this.ranges.yaxis.to
+                    }
+                });
+            }
+
+        },
+
+        // Depending on log type, we need to pack our data differently...
+        packData: function () {
+            var self = this;
+            // Create a table of Y values with the x values from our collection
+            var data = [];
+            var logs = this.deviceLogs;
+            // At this stage we know the logs are already fetched, btw
+            for (var j = 0; j < logs.length; j++) {
+                var ret = [];
+                var value = logs.at(j).entries;
+                var type = logs.at(j).get('logtype');
+                for (var i = 0; i < value.length; i++) {
+                    var entry = value.at(i);
+                    // Be sure we only plot CPM entries (we might have anything in the
+                    // log...
+                    var stamp = new Date(entry.get('timestamp')).getTime();
+                    if (entry.get('data').cpm != undefined) {
+                        var cpm = parseFloat((type == 'onyxlog') ? entry.get('data').cpm : entry.get('data').cpm.value);
+                        this.plot.fastAppendPoint({
+                            name: 'CPM',
+                            value: cpm,
+                            timestamp: stamp
+                        });
+                        if (this.overview)
+                            this.overview.fastAppendPoint({
+                                name: 'CPM',
+                                value: cpm,
+                                timestamp: stamp
+                            });
+                    }
+                }
+                if (ret.length)
+                    data.push({
+                        data: ret,
+                        label: "CPM"
+                    });
+            }
+            this.plot.redraw();
+            if (this.overview)
+                this.overview.redraw();
+            return data;
+        },
+
+        // Ugly at this stage, just to make it work (from flotcharts.org examples)
+        showTooltip: function (x, y, contents) {
+            $("<div id='tooltip' class='well'>" + contents + "</div>").css({
+                position: "absolute",
+                display: "none",
+                top: y + 5,
+                left: x + 5,
+                padding: "3px",
+                opacity: 0.90
+            }).appendTo("body").fadeIn(200);
+        },
+
+    });
+});
---
+++ server/www/js/app/lib/fileutils.js
@@ -0,0 +1,97 @@
+/**
+ * (c) 2015 Edouard Lafargue, ed@lafargue.name
+ *
+ * This file is part of Wizkers.
+ *
+ * Wizkers is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wizkers is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Wizkers.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  Various useful utility functions for writing to files. This is the version
+ *  that is compatible with the Apache Cordova framework.
+ *
+ * Original code (c) 2014 Edouard Lafargue, ed@lafargue.name
+ * All rights reserved.
+ */
+
+define(function (require) {
+
+    "use strict";
+
+    var WIZKERS_DIR = 'wizkers';
+    var WIZKERS_LOGS_DIR = 'logs';
+
+    // As per http://www.html5rocks.com/en/tutorials/file/filesystem/
+    function createDir(rootDirEntry, folders, cb) {
+        // Throw out './' or '/' and move on to prevent something like '/foo/.//bar'.
+        if (folders[0] == '.' || folders[0] == '') {
+            folders = folders.slice(1);
+        }
+        rootDirEntry.getDirectory(folders[0], {
+            create: true
+        }, function (dirEntry) {
+            // Recursively add the new subfolder (if we still have another to create).
+            if (folders.length > 1) {
+                createDir(dirEntry, folders.slice(1), cb);
+            } else {
+                cb(dirEntry);
+            }
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+
+
+    var fileUtils = function () {
+        var currentFile;
+
+        //////
+        // Public methods
+        /////
+        this.newLogFile = function (filename, callback) {
+            var dirpath = [WIZKERS_DIR, WIZKERS_LOGS_DIR];
+            resolveLocalFileSystemURL(cordova.file.externalRootDirectory, function (rootDirEntry) {
+                createDir(rootDirEntry, dirpath, function (dir) {
+                    dir.getFile("log.txt", {
+                        create: true
+                    }, function (file) {
+                        callback(file);
+                    });
+                });
+            });
+        };
+
+        /**
+         * Write a single line in a previously open log file
+         * @param {[[Type]]} entry [[Description]]
+         */
+        this.writeLog = function (file, entry) {
+            file.createWriter(function (fileWriter) {
+                fileWriter.seek(fileWriter.length);
+                var blob = new Blob([log], {
+                    type: 'text/plain'
+                });
+                fileWriter.write(blob);
+            }, function (e) {
+                console.log(e);
+            });
+        };
+        
+        
+        
+    }
+
+    return new fileUtils;
+});
