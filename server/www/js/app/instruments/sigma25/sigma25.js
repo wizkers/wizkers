3095aa24e4f381537ae2e71e5840b06fd86ffde6
Ed <edouard@lafargue.name>
2015-11-23 23:00:00 -0800

    Bug fixes on Live View/Num View references in instrument manager

--- server/www/js/app/instruments/sigma25/sigma25.js
+++ server/www/js/app/instruments/sigma25/sigma25.js
@@ -24,42 +24,43 @@
  * @author Edouard Lafargue, ed@lafargue.name
  */
 
-define(function(require) {
+define(function (require) {
     "use strict";
-    
-    var driver_frontend = require('app/instruments/sigma25/driver_frontend');
-    // Convenient function when views want to talk to each other: keep a central
-    // reference to those here
-    var current_liveview = null;
-    var current_numview = null;
 
-    return  function() {
-        
-        this.liveViewRef = function() {
+    var driver_frontend = require('app/instruments/sigma25/driver_frontend');
+
+    return function () {
+
+        // Convenient function when views want to talk to each other: keep a central
+        // reference to those here
+        var current_liveview = null;
+        var current_numview = null;
+
+        this.liveViewRef = function () {
             return current_liveview;
         };
-        
-        this.numViewRef = function() {
+
+        this.numViewRef = function () {
             return current_numview;
         };
 
         // Helper function: get driver capabilites for display.
         // returns a simple array of capabilities    
-        this.getCaps = function() {
+        this.getCaps = function () {
             return ["LiveDisplay", "NumDisplay", 'DiagDisplay',
                    ];
         };
-        
+
         // Return the type of data reading that this instrument generates. Can be used
         // by output plugins to accept data from this instrument or not.
-        this.getDataType = function() {
-                    return [];
+        this.getDataType = function () {
+            return [];
         }
-        
+
         // This has to be a Backbone view
         // This is the full screen live view graph (not a small widget)
-        this.getLiveDisplay = function(arg, callback) {
-            require(['app/instruments/sigma25/display_live'], function(view) {
+        this.getLiveDisplay = function (arg, callback) {
+            require(['app/instruments/sigma25/display_live'], function (view) {
                 current_liveview = new view(arg);
                 callback(current_liveview);
             });
@@ -67,61 +68,59 @@ define(function(require) {
 
         // This is a Backbone view
         // This is a numeric display
-        this.getNumDisplay = function(arg, callback) {
-            require(['app/instruments/sigma25/display_numeric'], function(view) {
+        this.getNumDisplay = function (arg, callback) {
+            require(['app/instruments/sigma25/display_numeric'], function (view) {
                 current_numview = new view(arg);
                 callback(current_numview);
             });
         };
 
         // A smaller widget (just a graph)
-        this.getLiveWidget = function(arg, callback) {
+        this.getLiveWidget = function (arg, callback) {
             return null;
         };
 
         // A diagnostics/device setup screen
-        this.getDiagDisplay = function(arg, callback) {
-            require(['app/instruments/sigma25/display_diag'], function(view) {
+        this.getDiagDisplay = function (arg, callback) {
+            require(['app/instruments/sigma25/display_diag'], function (view) {
                 callback(new view(arg));
             });
         };
 
         // This has to be a link manager
-        this.getDriver = function() {
+        this.getDriver = function () {
             return new driver_frontend();
         };
-        
-        this.getUploader = function() {
+
+        this.getUploader = function () {
             return new uploader_frontend();
         };
-        
+
         // This is a browser implementation of the backend driver, when we
         // run the app fully in-browser on as a Cordova native app.
         //
-        this.getBackendDriver = function(arg, callback) {
-            require(['app/instruments/sigma25/driver_backend'], function(view) {
+        this.getBackendDriver = function (arg, callback) {
+            require(['app/instruments/sigma25/driver_backend'], function (view) {
                 callback(new view(arg));
             });
         };
-        
+
         // Return a Backbone view which is a mini graph
-        this.getMiniLogview = function(arg, callback) {
+        this.getMiniLogview = function (arg, callback) {
             return null;
         };
 
         // Return a device log management view
-        this.getLogManagementView = function(arg, callback) {
-        }
+        this.getLogManagementView = function (arg, callback) {}
 
         // Render a log (or list of logs) for the device.
-        this.getLogView = function(arg, callback) {
-            require(['app/instruments/sigma25/display_log'], function(view) {
+        this.getLogView = function (arg, callback) {
+            require(['app/instruments/sigma25/display_log'], function (view) {
                 callback(new view(arg));
             });
         }
 
         // Render a log edit table for a log collection for the device
-        this.getLogEditView = function(arg, callback) {
-        }
+        this.getLogEditView = function (arg, callback) {}
     };
 });
