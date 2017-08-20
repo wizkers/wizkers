7ada0a96d4f7c3b47b54a05e5e212641398aea7e
elafargue <ed@wizkers.io>
2017-08-20 09:06:21 -0700

    Heartbeat monitor scaffolding - not functional yet

---
+++ wizkers/oem/bench/www/js/app/instruments/heartbeat/display_live.js
@@ -0,0 +1,356 @@
+/**
+ * This file is part of Wizkers.io
+ *
+ * The MIT License (MIT)
+ *  Copyright (c) 2016 Edouard Lafargue, ed@wizkers.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Live view display of the output of the Onyx. This live view is used
+ * by most Geiger instruments and is very configurable.
+ *
+ * @author Edouard Lafargue, ed@lafargue.name
+ */
+
+define(function (require) {
+    "use strict";
+
+    var $ = require('jquery'),
+        _ = require('underscore'),
+        Backbone = require('backbone'),
+        utils = require('app/utils'),
+        simpleplot = require('app/lib/flotplot'),
+        mapWidget = require('app/lib/mapwidget'),
+        template = require('js/tpl/instruments/heartbeat/LiveView.js');
+
+    return Backbone.View.extend({
+
+        initialize: function (options) {
+
+            this.currentDevice = null;
+            this.showstream = settings.get('showstream');
+
+            this.deviceinitdone = false;
+            this.plotavg = false;
+
+            // Get frequency and span if specified:
+            var span = this.model.get('liveviewspan'); // In seconds
+            var period = this.model.get('liveviewperiod'); // Polling frequency
+
+            var livepoints = 300; // 5 minutes @ 1 Hz
+            if (span && period) {
+                livepoints = span / period;
+            }
+
+            this.display_map = false;
+            this.display_graph = true;
+            if (vizapp.type == 'cordova') {
+                var wz_settings = instrumentManager.getInstrument().get('wizkers_settings');
+                if (wz_settings) {
+                    if (wz_settings.display_gmaps == 'true')
+                        this.display_map = true;
+                    if (wz_settings.display_graph == 'false')
+                        this.display_graph = false;
+                    if (wz_settings.screen_no_dim == 'true') {
+                        keepscreenon.enable();
+                    } else {
+                        keepscreenon.disable();
+                    }
+                } else {
+                    // Happens when the user never explicitely set the map option
+                    this.display_map = true;
+                }
+            }
+
+            // We will pass this when we create plots, this is the global
+            // config for the look and feel of the plot
+            this.plotoptions = {
+                points: livepoints,
+                vertical_stretch_parent: true,
+                plot_options: {
+                    xaxis: {
+                        mode: "time",
+                        show: true,
+                        timeformat: "%H:%M",
+                        timezone: settings.get("timezone")
+                    },
+                    grid: {
+                        hoverable: true
+                    },
+                    legend: {
+                        position: "ne"
+                    }
+                }
+            };
+
+            // Keep an array for moving average over the last X samples
+            // In Live view, we fix this at 1 minute. In log management, we will
+            // make this configurable
+            this.movingAvgPoints = 60;
+            this.movingAvgData = []; // Note: used for the graph, this stores the result of the moving average
+            this.movingAvgData2 = []; // Note: used for the graph, this stores the result of the moving average
+
+            linkManager.on('status', this.updatestatus, this);
+            linkManager.on('input', this.showInput, this);
+
+        },
+
+
+        events: {
+            "click #setdevicetag": "setdevicetag",
+        },
+
+        render: function () {
+            var self = this;
+            console.log('Main render of Heartbeat live view');
+            this.$el.html(template());
+
+            // Hide the raw data stream if we don't want it
+            if (!this.showstream) {
+                $("#showstream", this.el).empty();
+            }
+
+            // If we dont' want the maps, then modify the style of the graph to make sure
+            // it occupies the whole space:
+            if (!this.display_map) {
+                $('#map_row', this.el).empty();
+            }
+
+            if (!this.display_graph) {
+                $('#geigerchart_row', this.el).empty();
+            }
+
+
+            if (this.display_graph)
+                this.addPlot();
+
+            if (this.display_map) {
+                this.map = new mapWidget();
+                if (this.map != null) {
+                    $('.map_container', this.el).append(this.map.el);
+                    this.map.render();
+                    // Now we want the map element to autostretch. A bit of queries and trickery here,
+                    // so that we resize exactly to the correct height:
+                    // - We know our offset (below the home view buttons) within the window ($(self.el).offset().top)
+                    // - We know the size of the chart: $('.geigerchart',self.el).parent().height()
+                    //     Note: .parent() is the enclosing .thumbnail
+                    // - Then, we know the size of the numview: $('#numview').height();
+                    // - Last, remove 50 pixels to account for all the margins around the map/divs/thumbnails
+                    // ... now do the equation
+                    //$('.map_container', this.el).css('height', this.$el.parent().css('height'));
+
+                    var self = this;
+                    var rsc = function () {
+                        // We want the chart to be 18% of the screen
+                        if (self.display_graph) {
+                            self.$('.geigerchart').height(window.innerHeight * 0.16);
+                            if (self.plot && self.plot.rsc)
+                                self.plot.rsc();
+                        }
+                        var chartheight = $('#geigerchart_row', self.el).outerHeight();
+                        var numviewheight = 0;
+                        // We want to take the numview height into account if screen is xs or sm
+                        if (utils.checkBreakpoint('xs') || utils.checkBreakpoint('sm'))
+                            numviewheight = $('#numview').outerHeight();
+                        var mapheight = window.innerHeight - $(self.el).offset().top - chartheight - numviewheight - 20;
+                        if (mapheight < 50) {
+                            self.$('#map_row').hide();
+                            // Readjust the graph to take the rest of the space:
+                            var chartheight = window.innerHeight - $(self.el).offset().top - numviewheight - 20;
+                            self.$('.geigerchart').height(chartheight);
+                            // Then tell the chart to resize itself
+                            if (self.plot && self.plot.rsc)
+                                self.plot.rsc();
+                        } else {
+                            self.$('#map_row').show();
+                            $('.map_container > .mapwidget', self.el).height(mapheight);
+                            self.map.resize(mapheight);
+                        }
+                    }
+                    if (this.rsc)
+                        $(window).off('resize', this.rsc);
+                    this.rsc = rsc;
+                    $(window).on('resize', this.rsc);
+                    rsc();
+                }
+            } else {
+                // Implement a resizer for the Geiger chart only
+                if (!this.display_graph)
+                    return;
+                var self = this;
+                var rsc = function () {
+                    var numviewheight = 0;
+                    // We want to take the numview height into account if screen is xs or sm
+                    if (utils.checkBreakpoint('xs') || utils.checkBreakpoint('sm'))
+                        numviewheight = $('#numview').outerHeight();
+                    var chartheight = window.innerHeight - $(self.el).offset().top - numviewheight - 55;
+                    $('.geigerchart', self.el).css('height', chartheight + 'px');
+                    // Then tell the chart to resize itself
+                    if (self.plot)
+                        self.plot.rsc();
+                }
+                if (this.rsc)
+                    $(window).off('resize', this.rsc);
+                this.rsc = rsc;
+                $(window).on('resize', this.rsc);
+                rsc();
+            }
+
+
+            return this;
+        },
+
+        addPlot: function () {
+            var self = this;
+            this.plot = new simpleplot({
+                model: this.model,
+                settings: this.plotoptions
+            });
+            if (this.plot != null) {
+                $('.geigerchart', this.el).append(this.plot.el);
+                this.plot.render();
+            }
+        },
+
+        clear: function () {
+            $('.geigerchart', this.el).empty();
+            this.addPlot();
+            this.suspendGraph = true;
+        },
+
+        onClose: function () {
+            console.log("Onyx live view closing...");
+            linkManager.off('status', this.updatestatus);
+            linkManager.off('input', this.showInput);
+            if (this.rsc)
+                $(window).off('resize', this.rsc);
+            if (this.plot)
+                this.plot.onClose();
+        },
+
+        movingAverager: function (newpoint, buffer) {
+
+            buffer.push(newpoint);
+
+            // Keep our data to the length we want
+            if (buffer.length >= this.movingAvgPoints)
+                buffer.shift();
+
+            // Now compute the average
+            var avg = 0;
+            for (var i = 0; i < buffer.length; i++) {
+                avg += buffer[i];
+            }
+            return avg / buffer.length;
+
+        },
+
+        setdevicetag: function () {
+            var tag = $('#devicetagfield', this.el).val();
+            linkManager.driver.setdevicetag(tag);
+            $('#dtModal', this.el).modal('hide');
+            // Need a small delay to let the Onyx store the tag
+            setTimeout(linkManager.driver.devicetag, 300);
+        },
+
+
+        updatestatus: function (data) {
+            // Either the port is open and we have not done our device init,
+            // or the port is closed and we have to reset the device init status
+            if (data.portopen && !this.deviceinitdone) {
+                linkManager.driver.ping();
+            } else if (!data.portopen) {
+                this.deviceinitdone = false;
+            }
+        },
+
+        disp_data: function (data, ts) {
+            if (!this.display_graph)
+                return;
+
+            if (data.cpm != undefined) {
+                var cpm = parseFloat(data.cpm.value);
+
+                var dp = {
+                    'name': data.cpm.name ? (data.cpm.name + ' (' + data.cpm.unit + ')')  : "CPM",
+                    'value': cpm,
+                    'timestamp': ts
+                };
+
+                (typeof ts != 'undefined') ? this.plot.fastAppendPoint(dp): this.plot.appendPoint(dp);
+                dp = {
+                    'name': (data.cpm.name || "CPM") + " (average)",
+                    'value': this.movingAverager(cpm, this.movingAvgData),
+                    'timestamp': ts
+                };
+                (typeof ts != 'undefined') ? this.plot.fastAppendPoint(dp): this.plot.appendPoint(dp);
+            }
+            if (data.cpm2 != undefined) {
+                var cpm2 = parseFloat(data.cpm2.value);
+                var dp = {
+                    'name': data.cpm2.name ? (data.cpm2.name + ' (' + data.cpm2.unit + ')')  : "CPM2",
+                    'value': cpm2,
+                    'timestamp': ts
+                };
+                (typeof ts != 'undefined') ? this.plot.fastAppendPoint(dp): this.plot.appendPoint(dp);
+                dp = {
+                    'name': (data.cpm2.name || "CPM") + " (average)",
+                    'value': this.movingAverager(cpm2, this.movingAvgData2),
+                    'timestamp': ts
+                };
+                (typeof ts != 'undefined') ? this.plot.fastAppendPoint(dp): this.plot.appendPoint(dp);
+            }
+        },
+
+        // We get there whenever we receive something from the serial port
+        showInput: function (data) {
+            var self = this;
+
+            if (data.replay_ts != undefined) {
+                this.suspend_graph = false;
+                this.disp_data(data.data, data.replay_ts);
+                return;
+            }
+
+            // We're waiting for a data replay
+            if (this.suspend_graph)
+                return;
+
+            if (this.showstream) {
+                // Update our raw data monitor
+                var i = $('#input', this.el);
+                var scroll = (i.val() + JSON.stringify(data) + '\n').split('\n');
+                // Keep max 50 lines:
+                if (scroll.length > 50) {
+                    scroll = scroll.slice(scroll.length - 50);
+                }
+                i.val(scroll.join('\n'));
+                // Autoscroll:
+                i.scrollTop(i[0].scrollHeight - i.height());
+            }
+
+            if (data.cpm == undefined)
+                return;
+
+            this.disp_data(data);
+
+        },
+    });
+});
---
+++ wizkers/oem/bench/www/js/app/instruments/heartbeat/driver_backend.js
@@ -0,0 +1,353 @@
+/**
+ * This file is part of Wizkers.io
+ *
+ * The MIT License (MIT)
+ *  Copyright (c) 2016 Edouard Lafargue, ed@wizkers.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+define(function (require) {
+    "use strict";
+
+    var Backbone = require('backbone'),
+        abutils = require('app/lib/abutils'),
+        btleConnection = require('connections/btle');
+
+
+    var parser = function (socket) {
+
+        var self = this,
+            socket = socket,
+            streaming = true,
+            port = null,
+            port_close_requested = false,
+            port_open_requested = false,
+            isopen = false,
+            lastUpdate = new Date().getTime();
+
+        var HEART_RATE_SERVICE_UUID = '180d';
+        var HEART_RATE_MEASUREMENT_UUID = '2a37';
+
+        // Device information  uuids:
+        var DEV_INFO='180a';
+        var MFG_NAME = '2a24';
+        var MODEL_NUMBER = '2a24';
+        var SERIAL_NUMBER = '2a25';
+
+        // If possible, we want to keep track of the location of the measurement.
+        var current_loc = null,
+            location_status = '',
+            watchid = null;
+
+
+        // Those variables are used to do our CPM calculations.
+        // We use a one-window mechanism.
+        var count_buffer = [];
+        var time_interval = 5; // seconds
+        var buffer_max = 90 / time_interval; // Number of samples in the buffer (seconds/time_interval)
+        var buffer_idx = 0;
+
+        /////////////
+        // Private methods
+        /////////////
+
+        /**
+         * Compare two BLE UUIDs in long and short (16 bit) formats
+         */
+        var sameUuid = function(u1, u2) {
+            u1 = u1.toLowerCase();
+            u2 = u2.toLowerCase();
+            if (u1 == u2)
+                return true;
+
+            if (u1 == u2.substr(4,4))
+                return true;
+
+            if (u2 == u1.substr(4,4))
+                return true;
+
+            return false;
+        }
+
+        var portSettings = function () {
+            return {
+                service_uuid: HEART_RATE_SERVICE_UUID,
+                characteristic_uuid: HEART_RATE_MEASUREMENT_UUID
+            }
+        };
+
+        // Format can act on incoming data from the counter, and then
+        // forwards the data to the app through a 'data' event.
+        var format = function (data) {
+            if (!data.value)
+                return;
+
+            var response = '';
+            var updt = new Date().getTime();
+
+            // The Blue Onyx simulates a Heart Rate monitor, hence the references
+            // to Heart Rate below.
+
+            // The Heart Rate Measurement Characteristic does not allow 'read'
+            // operations and its value can only be obtained via notifications, so the
+            // |value| field might be undefined here.
+
+            if (sameUuid(data.service,HEART_RATE_SERVICE_UUID) && sameUuid(data.characteristic,HEART_RATE_MEASUREMENT_UUID)) {
+                var valueBytes = new Uint8Array(data.value);
+                if (valueBytes.length < 2) {
+                    console.log('Invalid Heart Rate Measurement value');
+                    return;
+                }
+
+                // Keep track of the time since we last got a packet - if this is more than
+                // 6 seconds, then we missed one (or more) and the CPM count cannot be trusted
+                var valid = false;
+                if ((updt - lastUpdate) < 6000)
+                    valid = true;
+
+                lastUpdate = updt;
+
+                // The first byte is the counts in the last 5 seconds
+                var count = valueBytes[0] + (valueBytes[1] << 8);
+                var battery_ok = (valueBytes[2] == 0x01);
+
+                var cpm = updateCPM(count);
+
+                // This is the CPM to µSv/h conversion coefficient for the tube
+                // in the Blue Onyx. A proper µSv/h output should also take a conversion
+                // coefficient into account for calibration.
+                var conversionCoefficient = 0.00294;
+
+                var response = {
+                    cpm: {
+                        value: cpm.cpm,
+                        usv: cpm.cpm * conversionCoefficient,
+                        valid: valid && cpm.valid
+                    },
+                    cp5s: count,
+                    batt_ok: battery_ok,
+                    loc: current_loc,
+                    loc_status: location_status
+                };
+
+                self.trigger('data', response);
+            } else if (sameUuid(data.service, DEV_INFO) && sameUuid(data.characteristic, SERIAL_NUMBER)) {
+                // The SN of the Blue Onyx is the MAC
+                var res = '';
+                for (var i in data.value) {
+                    res += ('0' + data.value[i].toString(16)).slice(-2) + ':';
+                }
+                self.trigger('data', {
+                    uniqueID: res.substr(0,res.length-1)
+                });
+            }
+        };
+
+        /**
+         * Update the current CPM. This is a very simple CPM calculation
+         * algorithm, which uses a fixed averaging window length, and tube
+         * dead time error correction. It sets a "valid" flag that becomes true
+         * once we have enough sample to do the averaging we want.
+         *
+         * @param {Number} count Count in the last 5 seconds
+         */
+        var updateCPM = function (count) {
+
+            count_buffer[buffer_idx] = count;
+            buffer_idx = (buffer_idx + 1) % buffer_max;
+
+            var count = 0;
+            var i = 0;
+            var buffer_filled = true;
+            for (i = 0; i < buffer_max; i++) {
+                if (isNaN(count_buffer[i])) {
+                    buffer_filled = false;
+                    break;
+                }
+                count += count_buffer[i];
+            }
+
+            //	 deadtime compensation
+            var rcpm = count * 60 / ((i + 1) * time_interval);
+            return {
+                cpm: rcpm / (1 - rcpm * 1.8833e-6),
+                valid: buffer_filled
+            };
+        };
+
+        // Status returns an object that is concatenated with the
+        // global server status
+        var status = function (stat) {
+            port_open_requested = false;
+            console.log('Port status change', stat);
+            if (stat.openerror) {
+                // We could not open the port: warn through
+                // a 'data' messages
+                var resp = {
+                    openerror: true
+                };
+                if (stat.reason != undefined)
+                    resp.reason = stat.reason;
+                if (stat.description != undefined)
+                    resp.description = stat.description;
+                self.trigger('data', resp);
+                return;
+            }
+
+            if (stat.reconnecting != undefined) {
+                // Forward the message to front-end
+                self.trigger('data', {
+                    reconnecting: stat.reconnecting
+                });
+                return;
+            }
+
+            isopen = stat.portopen;
+            if (isopen) {
+                // Runs our "onOpen" initialization routine here
+                console.log("Our port is now open");
+
+                port.subscribe({
+                    service_uuid: HEART_RATE_SERVICE_UUID,
+                    characteristic_uuid: HEART_RATE_MEASUREMENT_UUID
+                });
+
+                lastUpdate = new Date().getTime();
+
+                // If we don't receive data within 5 seconds, then take action
+                // and attempt to put the Blue Onyx into streaming mode (it might
+                // be in bootloader mode, and this requires writing "0" to the
+                // relevant service/characteristic
+                setTimeout(function () {
+                    if ((new Date().getTime() - lastUpdate) > 6000) {
+                        console.log("Do a Onyx bootloader mode cancel");
+                        port.write([0x30], {
+                            service_uuid: HEART_RATE_SERVICE_UUID,
+                            characteristic_uuid: BLEONYX_MEASURING_MODE
+                        }, function (info) {
+                            console.log('callback from write', info);
+                        });
+                    }
+                }, 8000);
+
+                // Now regularly ask the navigator for current position and refresh map
+                if (watchid == null) {
+                    watchid = navigator.geolocation.watchPosition(newLocation, geolocationError, {
+                        maximumAge: 10000,
+                        timeout: 20000,
+                        enableHighAccuracy: true
+                    });
+                }
+            } else {
+                // We remove the listener so that the serial port can be GC'ed
+                if (port_close_requested) {
+                    port.off('status', stat);
+                    port_close_requested = false;
+                    if (watchid != null)
+                        navigator.geolocation.clearWatch(watchid);
+                }
+            }
+        };
+
+        var newLocation = function (loc) {
+            location_status = 'OK';
+            current_loc = {
+                coords: {
+                    accuracy: loc.coords.accuracy,
+                    altitude: loc.coords.altitude,
+                    altitudeAccuracy: loc.coords.altitudeAccuracy,
+                    latitude: loc.coords.latitude,
+                    longitude: loc.coords.longitude,
+                    heading: loc.coords.heading,
+                    speed: loc.coords.speed
+                }
+            };
+
+        }
+
+        var geolocationError = function (err) {
+            console.log('Location error', err);
+            if (err.code == 3) {
+                location_status = 'no fix (timeout)';
+            } else
+                location_status = err.message;
+        }
+
+        /////////////
+        // Public methods
+        /////////////
+
+        this.openPort = function (insid) {
+            port_open_requested = true;
+            var ins = instrumentManager.getInstrument();
+            if (port == null) {
+                port = new btleConnection(ins.get('port'), portSettings());
+                port.on('data', format);
+                port.on('status', status);
+            }
+            port.open();
+        };
+
+        this.closePort = function (data) {
+            // We need to remove all listeners otherwise the serial port
+            // will never be GC'ed
+            port.off('data', format);
+            port_close_requested = true;
+            port.close();
+        }
+
+        this.isOpen = function () {
+            return isopen;
+        }
+
+        this.isOpenPending = function () {
+            return port_open_requested;
+        }
+
+        this.getInstrumentId = function (arg) {};
+
+        this.isStreaming = function () {
+            return streaming;
+        };
+
+        // Called when the app needs a unique identifier.
+        // this is a standardized call across all drivers.
+        //
+        // TODO: Returns the instrument GUID.
+        this.sendUniqueID = function () {
+            port.read(DEV_INFO, SERIAL_NUMBER);
+        };
+
+        // period in seconds
+        this.startLiveStream = function (period) {};
+
+        this.stopLiveStream = function (args) {};
+
+        // output should return a string, and is used to format
+        // the data that is sent on the serial port, coming from the
+        // HTML interface.
+        this.output = function (data) {
+            //console.log('TX', data);
+            port.write(data);
+        };
+    }
+
+    _.extend(parser.prototype, Backbone.Events);
+    return parser;
+});
